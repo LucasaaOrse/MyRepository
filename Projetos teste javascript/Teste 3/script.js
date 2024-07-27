85ea65a95170933b32860cf8507119790a8d9dc8
LucasOrse <lucas.orse11@gmail.com>
2024-07-27 11:05:13 -0300

    Comitei

--- Projetos teste javascript/Teste 3/script.js
+++ Projetos teste javascript/Teste 3/script.js
@@ -1,2 +1,43 @@
 let num = document.getElementById('inum')
 let lista = document.getElementById('icaixa')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n ) {
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    }else {
+        return false
+    }
+
+}
+
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    }else {
+        return false
+    }
+
+}
+
+
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado`
+        lista.appendChild(item)
+
+    }else {
+        alert('Numero invalido ou ja presente na lista')
+
+    }
+    num.value = " "
+    num.focus()
+ }
+
+ function finalizar(){
+
+ 
+ }
