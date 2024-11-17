b21ccab6c821541570053d3cd02f2b75566692a7
LucasOrse <lucas.orse11@gmail.com>
2024-11-17 20:08:19 -0300

    a

--- Projetos Node/Teste 2/index.js
+++ Projetos Node/Teste 2/index.js
@@ -1,14 +1,23 @@
 const express = require("express") // importando o express
 const app = express() // criando o servidor
 const bodyParser = require("body-parser")
+const connection = require("./database/database")
 
 app.set('view engine', "ejs") // setando qual a engine vai renderizar nosso html
 
-app.use(bodyParser.urlencoded({extends: false}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use(express.static("public"))
 
+connection
+    .authenticate()
+    .then(() => {
+        console.log("conexão feita com sucesso")
+    }).catch((error) =>{
+        console.error();
+        
+    })
 app.get("/", (req, res) => {
     res.render("index")
 
