210d2f1752c8502a2c0070a6ea1b4cd061ae13a4
LucasOrse <lucas.orse11@gmail.com>
2024-12-15 21:38:11 -0300

    A

--- Projetos Node/Teste 2/articles/ArticlesController.js
+++ Projetos Node/Teste 2/articles/ArticlesController.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/articles", (req,res) => {
-    res.send("rota de artigos")
+router.get("/admin/articles/new", (req,res) => {
+    res.render("admin/articles/new")
 
 })
 
