0051940e703cfb886357bd7560ed77ebc200db25
LUCAS TEIXEIRA ORSE <Z501886@corp.clarobr>
2025-04-10 19:05:15 -0300

    Primeiro commit

--- Projetos Node/Projeto React/Projeto Pizzaria/frontend/src/services/api.ts
+++ Projetos Node/Projeto React/Projeto Pizzaria/frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-     baseURL: "https://my-repository-indol-one.vercel.app/"
+     baseURL: "pizzaria-backend-production-3bd4.up.railway.app"
 })
